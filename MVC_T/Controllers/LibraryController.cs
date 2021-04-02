83f96cd37faeda51f65953a1d26d196f9737d4bd
Carlos Subauste <Carlos Subauste@CARLOS>
2021-04-01 20:03:31 -0500

    Agregar archivos de proyecto.

---
+++ MVC_T/Controllers/LibraryController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_T.Controllers
+{
+    public class LibraryController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
